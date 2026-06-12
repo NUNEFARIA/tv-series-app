30d2376d4216a0e5dbf387f9b19b9b9d5b7d01d9
NUNES <nunesfariasgustavo@gmail.com>
2026-06-12 20:13:30 -0300

    feat: add user repository basic structure

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,6 +1,20 @@
 package repository;
 
 
+import model.entities.UserData;
+
 public class UserRepository {
+    
+    public UserData load() {
+        
+        // Read Json
+        
+    }
+    
+    public void save() {
+        
+        // Write Json
+        
+    }
 
 }
