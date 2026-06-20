80964f177ea3760960301c348348fee29e945e75
NUNES <nunesfariasgustavo@gmail.com>
2026-06-20 13:34:00 -0300

    feat: add show dto

---
+++ src/model/dtos/ShowDTO.java
@@ -0,0 +1,52 @@
+package model.dtos;
+
+import java.util.List;
+
+public class ShowDTO {
+
+    private int id;
+    private String name;
+    private String language;
+    private List<String> genres;
+    private RatingDTO rating;
+    private String status;
+    private String premiered;
+    private String ended;
+    private NetworkDTO network;
+
+    public int getId() {
+        return  id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public RatingDTO getRating() {
+        return rating;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPremiered() {
+        return premiered;
+    }
+
+    public String getEnded() {
+        return ended;
+    }
+
+    public NetworkDTO getNetwork() {
+        return network;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+}
