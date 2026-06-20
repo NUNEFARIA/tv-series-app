c8b13e94c9cd346420073d82db4452f33bcb645b
NUNES <nunesfariasgustavo@gmail.com>
2026-06-20 13:23:00 -0300

    feat: add tv maze search dto

--- src/model/dtos/TvMazeSearchDTO.java
+++ src/model/dtos/TvMazeSearchDTO.java
@@ -1,4 +1,15 @@
 package model.dtos;
 
 public class TvMazeSearchDTO {
+
+    private double score;
+    private ShowDTO show;
+
+    public double getScore() {
+        return this.score;
+    }
+
+    public ShowDTO getShow() {
+        return this.show;
+    }
 }
